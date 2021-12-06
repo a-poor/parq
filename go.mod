1c859820211da0fc69a80b716716d4d3143a08e4
a-poor <a-poor@users.noreply.github.com>
2021-12-05 21:10:33 -0800

    Starting to add CLI def

--- go.mod
+++ go.mod
@@ -3,13 +3,22 @@ module github.com/a-poor/parq
 go 1.17
 
 require (
+	github.com/fatih/color v1.13.0
+	github.com/rodaine/table v1.0.1
+	github.com/urfave/cli/v2 v2.3.0
 	github.com/xitongsys/parquet-go v1.6.1
 	github.com/xitongsys/parquet-go-source v0.0.0-20211010230925-397910c5e371
 )
 
 require (
 	github.com/apache/thrift v0.13.1-0.20201008052519-daf620915714 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.10.5 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pierrec/lz4/v4 v4.1.6 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 )
--- parq.go
+++ parq.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"reflect"
 
+	"github.com/fatih/color"
+	"github.com/rodaine/table"
+	"github.com/urfave/cli/v2"
 	"github.com/xitongsys/parquet-go-source/local"
 	"github.com/xitongsys/parquet-go/reader"
 )
 
 const TestFilePath = "examples/data/iris.parquet"
 
+// getStructFields returns a list of the names of
+// the fields in a struct.
 func getStructFields(t reflect.Type) []string {
 	var fields []string
 	for i := 0; i < t.NumField(); i++ {
@@ -19,8 +25,21 @@ func getStructFields(t reflect.Type) []string {
 	return fields
 }
 
-func main() {
-	fmt.Println("Starting...")
+func formatRow(cols []string, val reflect.Value) []interface{} {
+	var data []interface{}
+	for _, c := range cols {
+		v := val.FieldByName(c)
+		if v.IsNil() {
+			data = append(data, "")
+		} else {
+			data = append(data, v.Elem().Interface())
+		}
+	}
+	return data
+}
+
+func runSandbox() {
+	log.Println("Starting...")
 
 	fr, err := local.NewLocalFileReader(TestFilePath)
 	if err != nil {
@@ -36,6 +55,7 @@ func main() {
 
 	num := int(pr.GetNumRows())
 	log.Printf("There are %d rows in the file\n", num)
+	fmt.Println()
 
 	res, err := pr.ReadByNumber(num)
 	if err != nil {
@@ -43,25 +63,132 @@ func main() {
 		return
 	}
 
-	// log.Printf("%T\n", res)
-	log.Printf("%+v\n", res[0])
-
 	t := pr.ObjType
-	log.Printf("%+v\n", t)
-	fmt.Println()
 	fs := getStructFields(t)
-	fmt.Printf("Struct fields: %v\n", fs)
 
-	r0 := res[0]
-	e := reflect.ValueOf(r0)
-	for _, f := range fs {
-		v := e.FieldByName(f)
-		fmt.Printf("%s = %v\n", f, v.Elem().Interface())
+	ifs := make([]interface{}, len(fs))
+	for i, r := range fs {
+		ifs[i] = r
+	}
+	headerFmt := color.New(color.FgMagenta, color.Italic).SprintfFunc()
+	tbl := table.New(ifs...)
+	tbl.WithHeaderFormatter(headerFmt)
+
+	for i := 0; i < 5; i++ {
+		r0 := res[i]
+		e := reflect.ValueOf(r0)
+		row := formatRow(fs, e)
+		tbl.AddRow(row...)
 	}
 
-	// tbl := table.New("ID", "Name", "Score", "Added")
-	// fmt.Println(tbl)
+	tbl.Print()
 
 	pr.ReadStop()
 	fr.Close()
+}
+
+/*
+
+head
+tail
+random
+all / show
+convert (to/from parquet)
+schema (show the table schema aka column names and types)
+
+*/
+
+func main() {
+	app := &cli.App{
+		Name:  "parq",
+		Usage: "A tool for exploring parquet files",
+		Commands: []*cli.Command{
+			{
+				Name:      "head",
+				Usage:     "Shows the first n rows of a parquet file.",
+				ArgsUsage: "FILENAME",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:    "n-rows",
+						Value:   10,
+						Aliases: []string{"n"},
+						Usage:   "Number of rows to show",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					return nil
+				},
+			},
+			{
+				Name:      "tail",
+				Usage:     "Shows the last n rows of a parquet file",
+				ArgsUsage: "FILENAME",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:    "n-rows",
+						Value:   10,
+						Aliases: []string{"n"},
+						Usage:   "Number of rows to show",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					return nil
+				},
+			},
+			{
+				Name:      "random",
+				Usage:     "Shows the n random rows of a parquet file.",
+				ArgsUsage: "FILENAME",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:    "n-rows",
+						Value:   10,
+						Aliases: []string{"n"},
+						Usage:   "Number of rows to show",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					return nil
+				},
+			},
+			{
+				Name:      "show",
+				Usage:     "Shows all rows of a parquet file.",
+				ArgsUsage: "FILENAME",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:    "n-rows",
+						Value:   10,
+						Aliases: []string{"n"},
+						Usage:   "Number of rows to show",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					return nil
+				},
+			},
+			{
+				Name:      "about",
+				Usage:     "Shows a parquet file's column names and data types.",
+				ArgsUsage: "FILENAME",
+				Flags:     []cli.Flag{},
+				Action: func(c *cli.Context) error {
+					return nil
+				},
+			},
+			{
+				Name:      "convert",
+				Usage:     "Convert a parquet file to another format.",
+				ArgsUsage: "FILENAME",
+				Flags:     []cli.Flag{},
+				Action: func(c *cli.Context) error {
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
